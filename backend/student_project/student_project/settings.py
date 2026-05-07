0461bea322991424d404fecdfa60ed756327a3cd
Jeckoamala04 <jeckoamala@mita.in>
2026-05-07 16:52:36 +0530

    add

--- backend/student_project/student_project/settings.py
+++ backend/student_project/student_project/settings.py
@@ -87,14 +87,22 @@ WSGI_APPLICATION = 'student_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'studentmang',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
+# render use panne  for live
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'studentmang',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # pagignation 
